83a18dd13922c3aad94f3f6d59e7e01794f036cc
KennerEspinal <kennerespinalg@gmail.com>
2024-03-26 23:18:20 -0500

    Formularios Básicos terminados

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+const product = {
+  name: 'RTX 4080',
+  price: 1500,
+  stock: 5
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
+
+  // myForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   stock: new FormControl(0),
+  // });
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    // this.myForm.reset(product);
+  }
+
+  isValidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  }
+
+  getErrorMessage(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'This field is required';
+        case 'minlength':
+          return `This field must have at least ${errors['minlength'].requiredLength} characters`;
+        case 'min':
+          return 'This field must be greater than 0';
+        default:
+          return null;
+      }
+    }
+    return null;
+  }
+
+
+  myForm: FormGroup = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    stock: [0, [Validators.required, Validators.min(0)]],
+  });
+
+  onSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+    this.myForm.reset({ price: 0, stock: 0 });
+  }
 
 }
